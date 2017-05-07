6b636226f13bae38a91ca5e90a5562d1033c66cf
Davide Urzino <davide.urzino@gmail.com>
2017-05-07 17:07:49 +0200

    bhoo

---
+++ Computer Sys/Sim_items.py
@@ -0,0 +1,54 @@
+from pyspark import SparkContext
+from pyspark.mllib.recommendation import ALS, MatrixFactorizationModel, Rating
+from pyspark.mllib.linalg import Matrix, Matrices
+import csv
+import operator
+
+def countDistinct(rdd):
+    return rdd.distinct().count()
+
+def countSameFeatures(item1,item2):
+    intersection = set(item1[1]).intersection(item2[1])
+    return len(intersection)
+
+sc = SparkContext.getOrCreate()
+
+icm_rdd = sc.textFile("data/icm.csv")
+
+header = icm_rdd.first()
+
+print(header)
+print(icm_rdd.count())
+
+clean_data = icm_rdd.filter(lambda x: x != header).map(lambda line: line.split(','))#.filter(lambda x: float(x[2]) >= 8)
+
+print(clean_data.take(10))
+
+features = clean_data.map(lambda x: int(x[1]))
+
+items = clean_data.map(lambda x: int(x[0]))
+
+dist_features=features.distinct()
+print("{0} distinct items and {1} distinct features".format(countDistinct(items), countDistinct(features)))
+
+item_features = clean_data.map(lambda x: (int(x[0]),int(x[1]))).groupByKey().map(lambda x: (x[0], list(x[1])))
+item_features.take(10)
+
+
+f = open('KNN_ITEMS.csv', 'wt')
+try:
+    writer = csv.writer(f)
+    writer.writerow(('itemId','neighborId','weight'))
+    for itm1 in item_features.take(10):
+        dict={}
+        for itm2 in item_features.take(15):
+
+            if itm1[0]!=itm2[0]:
+                dict[itm2[0]]= countSameFeatures(itm1,itm2)
+        dict=sorted(dict.items(),key=operator.itemgetter(1),reverse=True)
+
+
+        for (id, weight) in dict:
+            writer.writerow((itm1[0], id, weight))
+finally:
+    f.close()
