5974f9347fd7d63504e679dbeb6d6935b8679a42
Davide Urzino <davide.urzino@gmail.com>
2017-05-08 12:19:20 +0200

    uwiufisdofsio

--- Computer Sys/Item-Item-Matrix.py
+++ Computer Sys/Item-Item-Matrix.py
@@ -3,35 +3,49 @@ import csv
 from pyspark.mllib.linalg import Matrix, Matrices
 from scipy import linalg
 import numpy as np
+
 sc = SparkContext.getOrCreate()
-
-
-
 
 train_rdd = sc.textFile("data/train.csv")
 icm_rdd = sc.textFile("data/icm.csv")
+test_rdd= sc.textFile("data/test.csv")
 
 train_header = train_rdd.first()
 icm_header = icm_rdd.first()
+test_header= test_rdd.first()
 
-print(train_header)
-print(train_rdd.count())
-print(icm_header)
-print(icm_rdd.count())
-
-train_clean_data = train_rdd.filter(lambda x: x != train_header).map(lambda line: line.split(',')).map(lambda x: (int(x[0]), int(x[1]), float(x[2])))
+train_clean_data = train_rdd.filter(lambda x: x != train_header).map(lambda line: line.split(',')).map(lambda x: (int(x[0]), int(x[1]), int(x[2])))
 icm_clean_data = icm_rdd.filter(lambda x: x != icm_header).map(lambda line: line.split(',')).map(lambda x: (int(x[0]), int(x[1])))
+test_clean_data= test_rdd.filter(lambda x: x != test_header).map(lambda line: line.split(','))
 
 user_array = train_clean_data.map( lambda x: int(x[0])).sortBy(lambda x: x, ascending=False)
 item_array= icm_clean_data.map( lambda x: int(x[0])).sortBy(lambda x: x, ascending=False)
 features_array = icm_clean_data.map( lambda x: int(x[1])).sortBy(lambda x: x, ascending=False)
+useful_user_array=test_clean_data.map( lambda x: int(x[0]))
 
 U_dimension=user_array.take(1)[0]+2
 I_dimension=item_array.take(1)[0]+2
 F_dimension=features_array.take(1)[0]+2
-U_dimension
-I_dimension
-F_dimension
+
+for user in useful_user_array.toLocalIterator():
+
+
+    UxI_weighted=np.zeros(shape=(1,I_dimension))
+    UxI_one=np.zeros(shape=(1,I_dimension))
+    IxF=np.zeros(shape=(I_dimension,F_dimension))
+
+    for rate in train_clean_data.toLocalIterator():
+        if rate[0]==user:
+            UxI_weighted[0][rate[1]]=rate[2]
+    UxI_weighted
+    break
+
+UxI_weighted
+
+
+
+
+
 
 
 UxI_weighted=np.zeros(shape=(U_dimension,I_dimension))
@@ -43,8 +57,13 @@ for user in train_clean_data.toLocalIterator():
     UxI_weighted[user[0]][user[1]]=user[2]
     UxI_one[user[0]][user[1]]=1
 
+del train_clean_data
+
 for item in icm_clean_data.toLocalIterator():
     IxF[item[0]][item[1]]=1
+
+del icm_clean_data
+
 
 UxF=np.dot(UxI_weighted,IxF)
 UxF_counter=np.dot(UxI_one,IxF)
