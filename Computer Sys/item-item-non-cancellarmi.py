ba5b071b4b5e88f89962067a24347945f9500321
Federico Milani <federico@MacBook-Pro-di-Federico.local>
2017-05-09 13:37:53 +0200

    ajhgasasd

--- Computer Sys/item-item-non-cancellarmi.py
+++ Computer Sys/item-item-non-cancellarmi.py
@@ -23,27 +23,66 @@ item_array= icm_clean_data.map( lambda x: int(x[0])).sortBy(lambda x: x, ascendi
 features_array = icm_clean_data.map( lambda x: int(x[1])).sortBy(lambda x: x, ascending=False)
 useful_user_array=test_clean_data.map( lambda x: int(x[0]))
 
-U_dimension=user_array.take(1)[0]+2
+grouped_features = icm_clean_data.map(lambda x: (x[0],x[1])).groupByKey().map(lambda x: (x[0], list(x[1])))
+grouped_features.take(10)
+
+#U_dimension=user_array.take(1)[0]+2
 I_dimension=item_array.take(1)[0]+2
 F_dimension=features_array.take(1)[0]+2
 
-for user in useful_user_array.toLocalIterator():
+IxF=np.zeros(shape=(I_dimension,F_dimension))
+for item in icm_clean_data.toLocalIterator():
+    IxF[item[0]][item[1]] = 1
 
-    UxI_weighted=np.zeros(shape=(1,I_dimension))
-    UxI_one=np.zeros(shape=(1,I_dimension))
-    IxF=np.zeros(shape=(I_dimension,F_dimension))
+f = open('submission2.csv', 'wt')
+try:
+    writer = csv.writer(f)
+    writer.writerow(('userId','RecommendedItemIds'))
+    k = 0
+    for user in useful_user_array.toLocalIterator():
 
-    for rate in train_clean_data.toLocalIterator():
-        if rate[0]==user:
-            UxI_weighted[0][rate[1]]=rate[2]
-    UxI_weighted
-    break
+        UxI_weighted=np.zeros(shape=(1,I_dimension))
+        UxI_one=np.zeros(shape=(1,I_dimension))
 
-UxI_weighted
+        for rate in train_clean_data.toLocalIterator():
+            if rate[0]==user:
+                UxI_weighted[0][rate[1]]=rate[2]
+                UxI_one[0][rate[1]]=1
 
+        UxF=np.dot(UxI_weighted,IxF)
+        UxF_counter=np.dot(UxI_one,IxF)
+        for col in range(F_dimension):
+            if UxF_counter[0][col]!=0:
+                UxF[0][col]=UxF[0][col]/UxF_counter[0][col]
+
+        UxI_pred = np.dot(UxF, np.transpose(IxF))
+        for item in grouped_features.toLocalIterator():
+            UxI_pred[0][item[0]] = UxI_pred[0][item[0]] / len(item[1])
+
+        for i in range(I_dimension):
+            if UxI_one[0][i] == 1:
+                UxI_pred[0][i] == 0
+
+        predictions = np.zeros(shape=(1,5))
+        for i in range(5):
+            top_item = np.argmax(UxI_pred, axis = 1)[0]
+            predictions[0][i] = top_item
+            UxI_pred[0][top_item] = 0
+        #break
+        writer.writerow((user, '{0} {1} {2} {3} {4}'.format(predictions[0][0], predictions[0][1], predictions[0][2], predictions[0][3], predictions[0][4])))
+        k = k + 1
+        print(k)
+finally:
+    f.close()
+#UxI_pred
+#predictions
+#valore massimo per riga
+#np.argmax(UxI_pred, axis = 1)[0]
+'''
 UxI_weighted=np.zeros(shape=(U_dimension,I_dimension))
 UxI_one=np.zeros(shape=(U_dimension,I_dimension))
 IxF=np.zeros(shape=(I_dimension,F_dimension))
+
 
 for user in train_clean_data.toLocalIterator():
     UxI_weighted[user[0]][user[1]]=user[2]
@@ -64,7 +103,7 @@ for row in 0:U_dimension:
     for col in 0:F_dimension:
         if UxF_counter[row][col]!=0:
             UxF[row][col]=UxF[row][col]/UxF_counter[row][col]
-
+'''
 train_clean_data.take(10)
 icm_clean_data.take(10)
 
@@ -88,3 +127,6 @@ print(rating_array.count())
 items = icm_clean_data.map(lambda x: int(x[0]))
 total_items = items.distinct().count()
 print(total_items)
+
+#matrice iniziale di USERSxITEMS con valori di ratings (numero_righe, numero_col, colonne con valori, righe con valori, valori)
+user_item_rating_matrix = Matrices.sparse(total_users, total_items, item_array.collect(), user_array.collect(), rating_array.collect())
