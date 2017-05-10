5d7849ced82cb6965a3aa7d56f26be2317dd0400
Davide Urzino <davide.urzino@gmail.com>
2017-05-10 19:49:22 +0200

    Auto stash before merge of "master" and "origin/master"

--- Computer Sys/new_attempt.py
+++ Computer Sys/new_attempt.py
@@ -6,6 +6,12 @@ import numpy as np
 import math
 import random
 
+def getUserRatings(user,all_users):
+    for i in range(len(all_users)):
+        if all_users[i][0] != user:
+            all_users.remove(all_users[i])
+    return all_users
+
 
 
 def calculateDist(user1,user2):
@@ -21,7 +27,17 @@ def calculateDist(user1,user2):
         squared_distance_sum=squared_distance_sum + math.pow((rating1[2]-rating2.take(1)[0][2]),2)
         print (squared_distance_sum)'''
 
-    squared_distance_sum=random.uniform(0,90)
+
+
+
+    for i in range(len(us1)):
+        for j in range(len(us2)):
+            if us1[i][1]==us2[j][1]:
+
+                squared_distance_sum=squared_distance_sum + math.pow((us1[i][2]-us2[j][2]),2)
+
+
+    #squared_distance_sum=random.uniform(0,90)
 
     return  math.sqrt(squared_distance_sum)
 
@@ -29,21 +45,21 @@ def findKNN(K,user1,users_ratings,users_all):
     KNN = [0] * K
     KNN_closeness = [None] * K
 
-    user1_ratings = users_ratings.filter(lambda x: x[0]==user1)
+    user1_ratings = getUserRatings(user1,users_ratings)
 
     #se user1 non ha fatto ratings ritorna none
 
-    if user1_ratings.count == 0:
+    if len(user1_ratings) == 0:
         return None
 
-    for user2 in users_all.toLocalIterator():
-        user2_ratings= users_ratings.filter( lambda x: x[0] == user2)
+    for k in range(len(users_all)):
+        user2_ratings= getUserRatings(users_all[k],user_ratings.copy())
 
-        if user2_ratings.count == 0:
+        if len(user2_ratings) == 0:
             continue
         #se user2 non ha fatto rating non farci nulla
 
-        distance = calculateDist(user1_ratings,user2_ratings)
+        distance = calculateDist(user1_ratings,user2_ratings.copy())
 
 
         if 0 in KNN:
@@ -56,7 +72,7 @@ def findKNN(K,user1,users_ratings,users_all):
             actual_max_dist= max(KNN_closeness)
             for i in range(K):
                 if KNN_closeness[i]==actual_max_dist:
-                    KNN[i]=user2
+                    KNN[i]=users_all[k]
                     KNN_closeness[i]=distance
                     break
 
@@ -79,7 +95,7 @@ train_clean_data = train_rdd.filter(lambda x: x != train_header).map(lambda line
 icm_clean_data = icm_rdd.filter(lambda x: x != icm_header).map(lambda line: line.split(',')).map(lambda x: (int(x[0]), int(x[1])))
 test_clean_data= test_rdd.filter(lambda x: x != test_header).map(lambda line: line.split(','))
 
-user_array = train_clean_data.map( lambda x: int(x[0])).sortBy(lambda x: x, ascending=False)
+user_array = train_clean_data.map( lambda x: int(x[0])).sortBy(lambda x: x, ascending=False).distinct()
 item_array= icm_clean_data.map( lambda x: int(x[0])).sortBy(lambda x: x, ascending=False)
 features_array = icm_clean_data.map( lambda x: int(x[1])).sortBy(lambda x: x, ascending=False)
 useful_user_array=test_clean_data.map( lambda x: int(x[0])) #users to make recs
@@ -90,34 +106,44 @@ grouped_features = icm_clean_data.map(lambda x: (x[0],x[1])).groupByKey().map(la
 
 k=20
 
+train_data = train_clean_data.collect()
+users = user_array.collect()
 
-
-#U_dimension=user_array.take(1)[0]+2
-I_dimension=item_array.take(1)[0]+2
-F_dimension=features_array.take(1)[0]+2
-
-
-user1 = train_clean_data.filter(lambda x: x[0]==8)
-user1.take(100)
-user2 = train_clean_data.filter(lambda x: x[0]==28)
-user2.take(100)
-
-#begin test
-zaza = 0
-for rating1 in user1.toLocalIterator():
-    rating2 = user2.filter(lambda x: x[1]==rating1[1])
-    if rate.count() != 1:
-        continue
-
-
-    zaza=zaza + math.pow((rating1[2]-rating2.take(1)[0][2]),2)
-
-zaza
-#end test
 
 for user in useful_user_array.toLocalIterator():
 
 
-    KNN = findKNN(k,user,train_clean_data,user_array)
+    KNN = findKNN(k,user,train_data,users)
     break
 KNN
+
+
+
+
+
+
+
+
+user1 = train_clean_data.filter(lambda x: x[0]==4)
+user1.take(100)
+user2 = train_clean_data.filter(lambda x: x[0]==687)
+user2.take(100)
+
+#begin test
+us1=user1.collect()
+us2=user2.collect()
+
+us2[1]
+us2[1][1]
+
+len(us1)
+
+zaza = 0
+for i in range(len(us1)):
+    for j in range(len(us2)):
+        if us1[i][1]==us2[j][1]:
+
+            zaza=zaza + math.pow((us1[i][2]-us2[j][2]),2)
+
+zaza
+#end test
