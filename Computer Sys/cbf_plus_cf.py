4d350c75cf28d69539bc397cd36050f23e3a4d38
Davide Urzino <davide.urzino@gmail.com>
2017-05-20 13:48:05 +0200

    mastro

---
+++ Computer Sys/cbf_plus_cf.py
@@ -0,0 +1,115 @@
+from pyspark import SparkContext
+from scipy import sparse as sm
+from sklearn.preprocessing import normalize
+import numpy as np
+import csv
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy.stats import pearsonr as pears
+sc = SparkContext.getOrCreate()
+
+train_rdd = sc.textFile("data/train.csv")
+icm_rdd = sc.textFile("data/icm_fede.csv")
+test_rdd= sc.textFile("data/target_users.csv")
+
+train_header = train_rdd.first()
+icm_header = icm_rdd.first()
+test_header= test_rdd.first()
+
+train_clean_data = train_rdd.filter(lambda x: x != train_header).map(lambda line: line.split(',')).map(lambda x: (int(x[0]), int(x[1]), float(x[2])))
+icm_clean_data = icm_rdd.filter(lambda x: x != icm_header).map(lambda line: line.split(',')).map(lambda x: (int(x[0]), int(x[1])))
+test_clean_data= test_rdd.filter(lambda x: x != test_header).map(lambda line: line.split(','))
+
+test_users=test_clean_data.map( lambda x: int(x[0])).collect()
+
+
+grouped_rates = train_clean_data.filter(lambda x: x[0] in test_users).map(lambda x: (x[0],x[1])).groupByKey().map(lambda x: (x[0], list(x[1]))).collect()
+grouped_rates_dic = dict(grouped_rates)
+
+
+item_ratings = train_clean_data.map(lambda x: (x[0], x[2])).aggregateByKey((0,0), lambda x,y: (x[0] + y, x[1] + 1),lambda x,y: (x[0] + y[0], x[1] + y[1]))
+user_ratings_mean = item_ratings.mapValues(lambda x: (x[0] / (x[1]))).collect()
+user_ratings_mean_dic=dict(user_ratings_mean)
+
+
+item_ratings_forTop = train_clean_data.map(lambda x: (x[1], x[2])).aggregateByKey((0,0), lambda x,y: (x[0] + y, x[1] + 1),lambda x,y: (x[0] + y[0], x[1] + y[1]))#.sortBy(lambda x: x[1][1], ascending=False)
+#item_ratings.take(10)
+shrinkage_factor = 5
+item_ratings_mean = item_ratings_forTop.mapValues(lambda x: (x[0] / (x[1] + shrinkage_factor))).sortBy(lambda x: x[1], ascending = False).map(lambda x: x[0]).collect()
+
+
+users = train_clean_data.map(lambda x: x[0]).collect()
+items = train_clean_data.map(lambda x: x[1]).collect()
+ratings = train_clean_data.map(lambda x: x[2]).collect()
+
+items_for_features= icm_clean_data.map(lambda x:x[0]).collect()
+features = icm_clean_data.map(lambda x:x[1]).collect()
+items_for_features.append(37142)
+features.append(0)
+
+
+unos=[1]*len(items_for_features)
+
+UxI= sm.csr_matrix((ratings, (users, items)))
+IxF= sm.csr_matrix((unos, (items_for_features, features)))
+
+UxI=[[5,0,0,2],
+     [1,2,3,4]]
+IxF=[[1,0,1,0,1]
+     [1,1,0,0,0],
+     [0,0,1,1,1],
+     [0,1,1,0,1]]
+UxI=sm.csr_matrix(UxI)
+IxF=sm.csr_matrix(IxF)
+'''content based part'''
+IxF_normalized=normalize(IxF,axis=1)
+NumItems,NumFeatures=IxF.shape
+NumFeatures
+IDF=[0]*NumFeatures
+for i in range(NumFeatures):
+    IDF[i]=np.log10(NumItems/len(IxF.getcol(i).nonzero()[1]))
+UxF=UxI.dot(IxF_normalized)
+FxI=IxF_normalized.multiply(IDF).T
+UxI_pred_CBF=UxF.dot(FxI)
+
+''''user based collaborative filtering part'''
+#unbias the UxI_pred from CBF
+numusers,numitems=UxI_pred_CBF.shape
+for i in range(numusers):
+    user_avg=UxI_pred_CBF.getrow(i).mean()
+    for j in range(numitems):
+        UxI_pred_CBF-=(user_avg)
+UxI_pred_CBF.toarray()
+
+UxU_sim=cosine_similarity(UxI_pred_CBF)
+UxU_sim.setdiag(0)
+UxI_pred=UxU_sim.dot(UxI)
+
+
+c=0
+f = open('submission_CBF_plus_CF.csv', 'wt')
+writer = csv.writer(f)
+writer.writerow(('userId','RecommendedItemIds'))
+for user in test_users:
+    top=[0,0,0,0,0]
+
+    user_predictions=UxI_pred.getrow(user)
+    iterator = 0
+    for i in range(5):
+        prediction = user_predictions.argmax()
+        while prediction in grouped_rates_dic[user] and prediction != 0:
+            user_predictions[0,prediction]=-9
+            prediction=user_predictions.argmax()
+        if prediction == 0:
+            prediction = item_ratings_mean[iterator]
+            while prediction in grouped_rates_dic[user] or prediction in top:
+                iterator += 1
+                prediction = item_ratings_mean[iterator]
+            iterator += 1
+        else:
+            user_predictions[0,prediction]=-9
+        top[i]=prediction
+    c+=1
+    print(c)
+    writer.writerow((user, '{0} {1} {2} {3} {4}'.format(top[0], top[1], top[2], top[3], top[4])))
+
+f.close()
